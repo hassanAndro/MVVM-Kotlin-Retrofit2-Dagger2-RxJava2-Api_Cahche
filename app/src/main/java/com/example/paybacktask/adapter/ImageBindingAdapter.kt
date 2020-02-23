32971c1f3820b16692f5bf8946dcd014a76e31c3
Hassan Hameed <hassanhameed028@gmail.com>
2020-02-23 18:27:31 +0100

    Loading Icon and Landscape mode support

--- app/src/main/java/com/example/paybacktask/adapter/ImageBindingAdapter.kt
+++ app/src/main/java/com/example/paybacktask/adapter/ImageBindingAdapter.kt
@@ -4,11 +4,25 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.example.paybacktask.R
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.Priority
+
 
 object ImageBindingAdapter {
     @JvmStatic
     @BindingAdapter("android:src")
     fun setImageUrl(view: ImageView, url: String) {
-        Glide.with(view.context).load(url).placeholder(R.drawable.default_image).into(view)
+
+        val options = RequestOptions()
+            .centerCrop()
+            .placeholder(R.drawable.progress_animation)
+            .error(R.drawable.default_image)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .priority(Priority.HIGH)
+            .dontAnimate()
+            .dontTransform()
+
+        Glide.with(view.context).load(url).apply(options).into(view)
     }
 }
